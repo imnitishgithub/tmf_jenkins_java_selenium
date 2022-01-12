392dab2505576c8d4ceed33f6aef90590b015f13
Nitish Gupta <36029518+imnitishgithub@users.noreply.github.com>
2022-01-12 11:31:10 +0530

    Created HelloWorld.java
    Created HelloWorld.java

---
+++ hello-world/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+          System.out.println("Wow... Our github code is running from Jenkins using DevOps CI and CD ....");     
+  }
+}
